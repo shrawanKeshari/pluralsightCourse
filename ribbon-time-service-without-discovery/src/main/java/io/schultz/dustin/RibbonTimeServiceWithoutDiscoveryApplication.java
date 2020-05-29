20a1280c7b80484b1e1117250d27a8417f8d21a4
shrawan.keshari <shrawan.keshari@infoedge.com>
2020-05-29 23:40:11 +0530

    Calling Services Using Client-side Load Balancing

---
+++ ribbon-time-service-without-discovery/src/main/java/io/schultz/dustin/RibbonTimeServiceWithoutDiscoveryApplication.java
@@ -0,0 +1,28 @@
+package io.schultz.dustin;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class RibbonTimeServiceWithoutDiscoveryApplication {
+
+	@Value("${server.port}")
+	private int port;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonTimeServiceWithoutDiscoveryApplication.class, args);
+	}
+	
+	@GetMapping
+	public String getTime() {
+		return "The current time is " + new Date().toString()
+				+ "(answered by service running on " + port + ")";
+	}
+
+}
